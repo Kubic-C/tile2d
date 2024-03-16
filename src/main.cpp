7b81ea55f6bd687a70b131958442694f8e627b31
Kubic <56777409+Kubic-C@users.noreply.github.com>
2024-03-16 02:53:26 -0500

    03 04 test only work on AA OBB

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 #include <SFML/Graphics.hpp>
@@ -11,6 +11,10 @@
 
 #include <boost/geometry.hpp>
 #include <boost/geometry/index/rtree.hpp>
+
+#include <boost/thread/latch.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio/post.hpp>
 
 using namespace std::chrono;
 
@@ -29,15 +33,15 @@ struct std::less<glm::i32vec2> {
 template<>
 struct std::hash<glm::i32vec2> {
 	bool operator()(const glm::i32vec2& vec) const {
-		float _1i = (_1.y * width) + _1.x;
-		float _2i = (_2.y * width) + _2.x;
+		const int32_t p1 = 73856093;
+		const int32_t p2 = 19349663;
 
-		return _1i < _2i;
+		return vec.x * p1 ^ vec.y * p2 ;
 	}
 };
 
 template<typename K, typename T>
-using Map = std::map<K, T>;
+using Map = std::unordered_map<K, T>;
 
 namespace bgi = boost::geometry::index;
 static constexpr size_t dimension = 2;
@@ -66,6 +70,52 @@ struct {
 	float collisionResTotalTime = 0.0f;
 } stats;
 
+struct SinCos {
+	SinCos()
+		: sin(0.0f), cos(1.0f) {}
+
+	SinCos(float rot) {
+		setRot(rot);
+	}
+
+	SinCos(float psin, float pcos)
+		: sin(psin), cos(pcos) {}
+
+	void setRot(float rot) {
+		sin = glm::sin(rot);
+		cos = glm::cos(rot);
+	}
+
+	SinCos getNegate() const {
+		return SinCos(-sin, cos);
+	}
+
+	SinCos operator-(const SinCos& other) const {
+		//sin(a−c)=sin(a)cos(c)−cos(a)sin(c)
+		//cos(a−c)=cos(a)cos(c)+sin(a)sin(c)
+
+		SinCos dif;
+		dif.sin = sin * other.cos - cos * other.sin;
+		dif.cos = cos * other.cos + sin * other.sin;
+		
+		return dif;
+	}
+
+	SinCos operator+(const SinCos& other) const {
+		//sin(a+c)=sin(a)cos(c)+cos(a)sin(c)
+		//cos(a+c)=cos(a)cos(c)−sin(a)sin(c)
+
+		SinCos dif;
+		dif.sin = sin * other.cos + cos * other.sin;
+		dif.cos = cos * other.cos - sin * other.sin;
+
+		return dif;
+	}
+
+	float sin;
+	float cos;
+};
+
 inline glm::vec2 rotate(const glm::vec2& v, float sin, float cos) {
 	glm::vec2 result;
 
@@ -74,9 +124,48 @@ inline glm::vec2 rotate(const glm::vec2& v, float sin, float cos) {
 	return result;
 }
 
+inline glm::vec2 rotate(const glm::vec2& v, const SinCos& sincos) {
+	return rotate(v, sincos.sin, sincos.cos);
+}
+
 sf::Vector2f convert(const glm::vec2& vec) {
 	return sf::Vector2f(vec.x, vec.y);
 }
+
+struct Transform {
+	using vec2 = glm::vec<2, float>;
+
+	Transform()
+		: pos(0.0f, 0.0f), rot(0.0f) {}
+
+	Transform(const vec2& pos, float rot)
+		: pos(pos), rot(rot) {}
+
+	Transform getInverse() const {
+		return Transform(-pos, -rot);
+	}
+
+	vec2 getWorldPoint(const vec2& localPoint) {
+		return rotate(localPoint, sincos) + pos;
+	}
+
+	vec2 getWorldPoint(const vec2& localPoint, const glm::vec2& localOffset) {
+		return rotate(localPoint - localOffset, sincos) + localOffset + pos;
+	}
+
+	vec2 getLocalPoint(const vec2& worldPoint, const glm::vec2& localOffset) {
+		return rotate(worldPoint - pos - localOffset, sincos.getNegate()) + localOffset;
+	}
+
+	// updates cache of sincos
+	void update() {
+		sincos.setRot(rot);
+	}
+
+	vec2  pos; // In Meters
+	float rot; // In radians
+	SinCos sincos;
+};
 
 template<typename T = float>
 struct AABB {
@@ -86,10 +175,20 @@ public:
 	AABB()
 		: m_min(0, 0), m_max(0, 0) {}
 
-	AABB(vec2 pos, vec2 halfDim)
-		: m_min(-halfDim.x, -halfDim.y), m_max(halfDim.x, halfDim.y) {
-		
+	AABB(vec2 min, vec2 max)
+		: m_min(min), m_max(max) {}
+
+	AABB(vec2 pos, float hW, float hH)
+		: AABB({hW, hH}) {
 		*this += pos;
+	}
+
+	AABB(vec2 halfDim)
+		: m_min(-halfDim.x, -halfDim.y), m_max(halfDim.x, halfDim.y) {}
+
+
+	AABB(vec2 halfDim, const SinCos& sincos) {
+		*this = AABB<T>(halfDim).rotate(sincos);
 	}
 
 	AABB& operator+=(const vec2& off) {
@@ -97,6 +196,14 @@ public:
 		m_max += off;
 	
 		return *this;
+	}
+
+	AABB operator+(const vec2& off) const {
+		AABB newAABB = *this;
+		newAABB.m_min += off;
+		newAABB.m_max += off;
+
+		return newAABB;
 	}
 
 	AABB operator-(const vec2& off) const {
@@ -143,6 +250,10 @@ public:
 		callback(tl());
 	}
 
+	vec2 midpoint() const {
+		return (m_max + m_min) / (T)2;
+	}
+
 	T width() const {
 		return glm::abs(m_max.x - m_min.x);
 	}
@@ -152,9 +263,9 @@ public:
 	}
 
 	bool intersects(const AABB& other) const {
-		glm::vec2 d1, d2;
-		d1 = glm::vec2(other.m_min.x, other.m_min.y) - glm::vec2(m_max.x, m_max.y);
-		d2 = glm::vec2(m_min.x, m_min.y) - glm::vec2(other.m_max.x, other.m_max.y);
+		vec2 d1, d2;
+		d1 = vec2(other.m_min.x, other.m_min.y) - vec2(m_max.x, m_max.y);
+		d2 = vec2(m_min.x, m_min.y) - vec2(other.m_max.x, other.m_max.y);
 
 		if (d1.x > 0.0f || d1.y > 0.0f)
 			return false;
@@ -167,6 +278,25 @@ public:
 
 	Box getBoostBox() const {
 		return Box(Point(m_min.x, m_min.y), Point(m_max.x, m_max.y));
+	}
+
+	// When T is an integer type, rotation will be unstable.
+	AABB<T> rotate(T sin, T cos) const {
+		sin = glm::abs(sin);
+		cos = glm::abs(cos); 
+
+		T hWidth = width() / (T)2;
+		T hHeight = height() / (T)2;
+
+		return {
+			midpoint(), 
+			hHeight * sin + hWidth * cos, 
+			hWidth * sin + hHeight * cos
+		};
+	}
+
+	AABB<T> rotate(const SinCos& sincos) const {
+		return rotate(sincos.sin, sincos.cos);
 	}
 
 private:
@@ -222,6 +352,9 @@ struct SpatialTileIndexEqualTo {
 	}
 };
 
+using Rtree = bgi::rtree<SpatialTileIndex, bgi::linear<16, 8>,
+	SpatialTileIndexGetter, SpatialTileIndexEqualTo>;
+
 struct TileBody {
 	//float minQuadTree[2] = {-100000.0f, -100000.0f};
 	//float maxQuadTree[2] = { 100000.0f, 100000.0f };
@@ -233,10 +366,10 @@ public:
 	};
 
 	TileBody() 
-		: inverseI(0.0f), inverseMass(0.0f), rotSin(0.0f), rotCos(0.0f),
-		mass(0.0f), I(0.0f), com(0.0f, 0.0f), unweightedCom(0.0f, 0.0f),
-		pos(0.0f, 0.0f), linearVelocity(0.0f, 0.0f), force(0.0f, 0.0f),
-		rot(0.0f), angularVelocity(0.0f), torque(0.0f), normals(defaultNormals) {
+		: inverseI(0.0f), inverseMass(0.0f),
+		mass(0.0f), I(0.0f), com(0.0f, 0.0f), unweightedCom(0.0f, 0.0f), 
+		linearVelocity(0.0f, 0.0f), force(0.0f, 0.0f), angularVelocity(0.0f), 
+		torque(0.0f), normals(defaultNormals) {
 		cache.clear();
 	}
 
@@ -248,30 +381,28 @@ public:
 		// P = p + V * dt;
 
 		linearVelocity = linearVelocity + force * inverseMass * dt;
-		pos = pos + linearVelocity * dt;
+		m_transform.pos = m_transform.pos + linearVelocity * dt;
 		force = {0.0f, 0.0f};
 
 		angularVelocity = angularVelocity + torque * inverseI * dt;
-		rot = rot + angularVelocity * dt;
+		m_transform.rot = m_transform.rot + angularVelocity * dt;
 		torque = 0.0f;
 
-		rotSin = glm::sin(rot);
-		rotCos = glm::cos(rot);
-		normals[0] = rotate(defaultNormals[0], rotSin, rotCos); 
-		normals[1] = rotate(defaultNormals[1], rotSin, rotCos);
+		m_transform.update();
 
-		// https://i.stack.imgur.com/0SH6d.png
-		float hWidth  = (float)cache.corners.width() * 0.5f * Tile::width + Tile::width * 0.5f;
-		float hHeight = (float)cache.corners.height() * 0.5f * Tile::height + Tile::height * 0.5f;
+		normals[0] = rotate(defaultNormals[0], m_transform.sincos); 
+		normals[1] = rotate(defaultNormals[1], m_transform.sincos);
+
 		glm::vec2 dim = {
-			hHeight * abs(rotSin) + hWidth * abs(rotCos),
-			hWidth  * abs(rotSin) + hHeight * abs(rotCos)
+			(float)cache.corners.width() * Tile::width * 0.5f,
+			(float)cache.corners.height() * Tile::height * 0.5f,
 		};
-		aabb = AABB(pos + com, dim);
+
+		aabb = AABB(dim).rotate(m_transform.sincos) + m_transform.pos + com;
 	}
 
 	void addTile(glm::i32vec2 iPos, float density = 1.0f, float staticFriction = 1.0f, float dynamicFriction = 1.0f, float E = 0.5f) {
-		assert(!tiles.contains(iPos));
+		//assert(!tiles.contains(iPos));
 
 		glm::vec2 pos = getTileLocalPoint(iPos);
 		tiles.insert(std::pair(iPos, Tile()));
@@ -355,23 +486,56 @@ public:
 		}
 	}
 
-	glm::vec2 getWorldPoint(const glm::vec2& localPoint) const {
-		glm::vec2 rot_point = rotate(localPoint - com, rotSin, rotCos);
-
-		return (rot_point + com) + pos;
+	Transform& transform() {
+		return m_transform;
 	}
 
-	glm::vec2 getLocalPoint(const glm::vec2& worldPoint) const {
-		glm::vec2 worldPos = pos + com;
+	void moveBy(const glm::vec2& addPos) {
+		m_transform.pos += addPos;
+	}
 
-		return worldPoint - worldPos;
+	void rotateBy(float amount) {
+		m_transform.rot += amount;
+		m_transform.update();
+	}
+
+	void setPos(const glm::vec2& pos) {
+		m_transform.pos = pos;
+	}
+
+	void setRot(float rot) {
+		m_transform.rot = rot;
+		m_transform.update();
+	}
+
+	glm::vec2 getWorldPoint(const glm::vec2& localPoint) {
+		return m_transform.getWorldPoint(localPoint, com);
+	} 
+
+	glm::vec2 getLocalPoint(const glm::vec2& worldPoint) {
+		return m_transform.getLocalPoint(worldPoint, com);
 	}
 
 	// converts a world aabb to a local aabb for use with colliding in the local spatial index tree
 	// this means its not entirely a true local conversion as it does not take into account
 	// Center of Mass.
 	AABB<float> getLocalAABB(const AABB<float>& worldAABB) {
-		return worldAABB - (pos);
+		AABB localAABB(getLocalPoint(worldAABB.midpoint()), worldAABB.width() * 0.5f, worldAABB.height() * 0.5f);
+
+		return localAABB.rotate(m_transform.sincos.getNegate());
+	}
+
+	// get the AABB in the localSpace of spaceTransform
+	AABB<float> getAABB(const Transform& spaceTransform, const glm::vec2& localOffset = {0.0f, 0.0f}) {
+		glm::vec2 dim = {
+			(float)cache.corners.width() * Tile::width * 0.5f,
+			(float)cache.corners.height() * Tile::height * 0.5f,
+		};
+
+		glm::vec2 relPos = (spaceTransform.pos + localOffset) - (m_transform.pos + com);
+		AABB aabb = AABB(dim, spaceTransform.sincos - m_transform.sincos) + relPos;
+
+		return aabb;
 	}
 
 	// get the local position of a tile
@@ -404,15 +568,8 @@ public:
 
 	AABB<float> getTileWorldAABB(glm::i32vec2 iPos) {
 		glm::vec2 tilePos = (glm::vec2)iPos * glm::vec2(Tile::width, Tile::height);
-
-		float hWidth = (float)0.5f * Tile::width;
-		float hHeight = (float)0.5f * Tile::height;
-		glm::vec2 dim = {
-			hHeight * abs(rotSin) + hWidth * abs(rotCos),
-			hWidth * abs(rotSin) + hHeight * abs(rotCos)
-		};
 		
-		return {getWorldPoint(tilePos), dim};
+		return AABB(getWorldPoint(tilePos), Tile::width * 0.5f, Tile::height * 0.5f).rotate(m_transform.sincos);
 	}
 
 	// Gets aabb in local space
@@ -420,28 +577,25 @@ public:
 		glm::vec2 pos = (glm::vec2)iPos * glm::vec2(Tile::width, Tile::height);
 		glm::vec2 halfTile = glm::vec2(Tile::width, Tile::height) * 0.5f;
 
-		return {pos, halfTile};
+		return {pos, Tile::width * 0.5f, Tile::height * 0.5};
 	}
 
 	Map<glm::i32vec2, Tile> tiles;
 
-	glm::vec2 com;
-	float rotSin;
-	float rotCos;
 private: 
 	float inverseI;
 	float inverseMass;
 
 private:
+	glm::vec2 com;
 	float mass;
 	float I;
 	glm::vec2 unweightedCom;
 
+	Transform m_transform;
 public: // integrating
-	glm::vec2 pos;
 	glm::vec2 linearVelocity;
 	glm::vec2 force;
-	float rot;
 	float angularVelocity;
 	float torque;
 
@@ -456,8 +610,7 @@ public: // integrating
 
 	AABB<float> aabb;
 	std::array<glm::vec2, 2> normals;
-	bgi::rtree<SpatialTileIndex, bgi::quadratic<16, 8>, 
-		SpatialTileIndexGetter, SpatialTileIndexEqualTo> tilePartition;
+	Rtree tilePartition;
 };
 
 using MinMax = std::pair<float, float>;
@@ -500,8 +653,8 @@ struct CollisionManifold {
 };
 
 void collisionResolve(TileBody& body1, TileBody& body2, const CollisionManifold& manifold) {
-	//body1.pos -= manifold.normal * manifold.depth * 0.5f;
-	//body2.pos += manifold.normal * manifold.depth * 0.5f;
+	body1.moveBy(-manifold.normal * manifold.depth * 0.5f);
+	body2.moveBy(manifold.normal * manifold.depth * 0.5f);
 }
 
 bool fineCollisionDetect(TileBody& body1, glm::i32vec2 tilePos1, TileBody& body2, glm::i32vec2 tilePos2, CollisionManifold& manifold) {
@@ -629,7 +782,7 @@ leave:
 // With tiles recieved from body1's and body2's tree query, use a O(n^2) loop
 // to test each tile from opposite lists against eachother using their WorldAABB before continuing
 // to a finer collision detection test (SAT).
-void testCollision_04(TileBody& body1, TileBody& body2) {
+void testCollision_04(TileBody& body1, TileBody& body2, AABB<float>& oApproxArea) {
 	Stopwatch timer;
 	timer.start();
 
@@ -639,6 +792,8 @@ void testCollision_04(TileBody& body1, TileBody& body2) {
 	AABB<float> approxArea;
 	if(!computeApproxCollisionArea(body1.aabb, body2.aabb, approxArea))
 		goto leave;
+
+	oApproxArea = approxArea;
 
 	AABB approxAreaBody1 = body1.getLocalAABB(approxArea);
 	body1.tilePartition.query(bgi::intersects(approxAreaBody1.getBoostBox()), std::back_inserter(tiles1));
@@ -664,6 +819,56 @@ leave:
 	stats.collisionDetCalled++;
 }
 
+// Test Collision 05 - O(n^2) MT Local R-Tree AABB skim + tile AABB - (Porter Tile Collision Detection):
+// Using the the AABB from body1 and body2, it tests if there could be a collision.
+// If there is a collision form a new AABB from body1's and body2's AABB 
+// using computeApproxCollisionArea() function, then convert this aabb into the local 
+// space of body1 and body2. With this new local aabb query into the local spatial 
+// index trees for any possible colliding tiles, do this for both body1 and body2. 
+// With tiles recieved from body1's and body2's tree query, use a O(n^2) loop
+// to test each tile from opposite lists against eachother using their WorldAABB before continuing
+// to a finer collision detection test (SAT).
+void testCollision_05(boost::asio::thread_pool& tp, TileBody& body1, TileBody& body2) {
+	Stopwatch timer;
+	timer.start();
+
+	std::vector<SpatialTileIndex> tiles1;
+	std::vector<SpatialTileIndex> tiles2;
+	boost::latch latch(2);
+	
+	AABB<float> approxArea;
+	if (!computeApproxCollisionArea(body1.aabb, body2.aabb, approxArea))
+		goto leave;
+
+	auto queryTree =
+		[&latch](const Box& AABB, Rtree& rtree, std::vector<SpatialTileIndex>& tiles) {
+			rtree.query(bgi::intersects(AABB), std::back_inserter(tiles));
+			latch.count_down();
+		};
+
+	queryTree(body1.getLocalAABB(approxArea).getBoostBox(), body1.tilePartition, tiles1);
+	queryTree(body2.getLocalAABB(approxArea).getBoostBox(), body2.tilePartition, tiles2);
+	latch.wait();
+
+	for (auto& tile1 : tiles1) {
+		auto tile1AABB = body1.getTileWorldAABB(tile1.tile);
+
+		for (auto& tile2 : tiles2) {
+			auto tile2AABB = body2.getTileWorldAABB(tile2.tile);
+			if (!tile1AABB.intersects(tile2AABB))
+				continue;
+
+			CollisionManifold manifold;
+			if (fineCollisionDetect(body1, tile1.tile, body2, tile2.tile, manifold))
+				collisionResolve(body1, body2, manifold);
+		}
+	}
+
+leave:
+	stats.collisionDetTotalTime += timer.getTime();
+	stats.collisionDetCalled++;
+}
+
 void drawAABB(sf::RenderWindow& window, AABB<float>& aabb, sf::Color outlineColor, float thickness) {
 	sf::ConvexShape shape(4);
 	shape.setPoint(0, convert(aabb.bl()));
@@ -677,6 +882,14 @@ void drawAABB(sf::RenderWindow& window, AABB<float>& aabb, sf::Color outlineColo
 }
 
 int main() {
+	boost::asio::thread_pool tp(4);
+
+	float a = 0.5f;
+	std::cout << "sin(a)" << glm::sin(a) << '\n';
+	std::cout << "sin(-a)" << glm::sin(-a) << '\n';
+	std::cout << "-sin(a)" << -glm::sin(a) << '\n';
+
+
 	int testSizes[] = {
 		5,
 		10,
@@ -687,7 +900,7 @@ int main() {
 	};
 	int currentTest = 0;
 	int maxTests = 6;
-	const float testLength = 5.0; // 5 seconds
+	const float testLength = 500.0; // 5 seconds
 
 	sf::RenderWindow window(sf::VideoMode({ 800U, 600U }), "Test Field", sf::State::Windowed);
 	for(; currentTest < maxTests; currentTest++) {
@@ -706,8 +919,10 @@ int main() {
 			}
 		}
 
-		body.pos = { 400, 300 };
-		body2.pos = { 400, 300 };
+		body.setPos({ 300, 300 });
+		body.setRot(glm::two_over_pi<float>() * 0.1f);
+		body2.setPos({ 400, 300 });
+		body2.setRot(glm::two_over_pi<float>());
 
 		steady_clock::time_point start = steady_clock::now();
 		while(window.isOpen() && duration_cast<seconds>(steady_clock::now() - start).count() < testLength) {
@@ -723,22 +938,24 @@ int main() {
 				}
 			}
 
-			float moveSpeed = 5.0f;
+			float moveSpeed = 0.1f;
 			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D)) {
-				body.pos.x += moveSpeed;
+				body.moveBy({moveSpeed, 0.0f});
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A)) {
-				body.pos.x -= moveSpeed;
+				body.moveBy({-moveSpeed, 0.0f});
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W)) {
-				body.pos.y -= moveSpeed;
+				body.moveBy({0.0f, -moveSpeed});
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)) {
-				body.pos.y += moveSpeed;
+				body.moveBy({0.0f, moveSpeed});
 			}
 
 			body.integrate(0.16f);
 			body2.integrate(0.16f);
+			
+			body.rotateBy(glm::two_over_pi<float>() * 0.02f);
 
 			window.clear();
 
@@ -767,7 +984,48 @@ int main() {
 			drawAABB(window, body.aabb, sf::Color::Blue, 2.0f);
 			drawAABB(window, body2.aabb, sf::Color::Blue, 2.0f);
 
-			testCollision_04(body, body2);
+			AABB approxArea;
+			testCollision_04(body, body2, approxArea);
+
+			drawAABB(window, approxArea, sf::Color(238, 210, 2), -2.5f);
+			
+
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Q)) {
+				{
+					AABB approxAreaBody1 = body.getLocalAABB(approxArea);
+					std::vector<SpatialTileIndex> tiles;
+					body.tilePartition.query(bgi::intersects(approxAreaBody1.getBoostBox()), std::back_inserter(tiles));
+					for(auto& tile : tiles) {
+						drawAABB(window, body.getTileWorldAABB(tile.tile), sf::Color::Red, -1.0f);
+					}
+					sf::ConvexShape shape(4);
+					shape.setPoint(0, convert(body.getWorldPoint(approxAreaBody1.bl())));
+					shape.setPoint(1, convert(body.getWorldPoint(approxAreaBody1.br())));
+					shape.setPoint(2, convert(body.getWorldPoint(approxAreaBody1.tr())));
+					shape.setPoint(3, convert(body.getWorldPoint(approxAreaBody1.tl())));
+					shape.setOutlineColor(sf::Color::Green);
+					shape.setFillColor(sf::Color::Transparent);
+					shape.setOutlineThickness(-2.0f);
+					window.draw(shape);
+				}
+				{
+					AABB approxAreaBody2 = body2.getLocalAABB(approxArea);
+					std::vector<SpatialTileIndex> tiles;
+					body2.tilePartition.query(bgi::intersects(approxAreaBody2.getBoostBox()), std::back_inserter(tiles));
+					for (auto& tile : tiles) {
+						drawAABB(window, body2.getTileWorldAABB(tile.tile), sf::Color::Red, -1.0f);
+					}
+					sf::ConvexShape shape(4);
+					shape.setPoint(0, convert(body2.getWorldPoint(approxAreaBody2.bl())));
+					shape.setPoint(1, convert(body2.getWorldPoint(approxAreaBody2.br())));
+					shape.setPoint(2, convert(body2.getWorldPoint(approxAreaBody2.tr())));
+					shape.setPoint(3, convert(body2.getWorldPoint(approxAreaBody2.tl())));
+					shape.setOutlineColor(sf::Color::Green);
+					shape.setFillColor(sf::Color::Transparent);
+					shape.setOutlineThickness(-2.0f);
+					window.draw(shape);
+				}
+			}
 
 			window.display();
 		}
